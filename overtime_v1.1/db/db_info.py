4f2e9c4f45474ab39d3f3d1030ea8458a3895ffb
Jaehoon Lee <myholyrain@gmail.com>
2024-05-14 08:35:12 +0900

    first commit

--- overtime_v1.1/db/db_info.py
+++ overtime_v1.1/db/db_info.py
@@ -1,9 +1,16 @@
 class Connect:
 
-    host = "125.143.154.68"
-    port = 3307
+    # # test server 개인용
+    # host = "125.143.154.68"
+    # port = 3307
+    # database = "webserver"
+    # username = "scott"
+    # password = "!Jhlee0809"        
+
+    # doochpump 화성공장 hr server 
+    host = "106.248.19.234"
+    port = 3306
     database = "webserver"
     username = "scott"
     password = "!Jhlee0809"        
-
         
