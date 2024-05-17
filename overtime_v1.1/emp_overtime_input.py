27bae65022e05ce890d076266d2249ecb6f20bbd
Jaehoon Lee <myholyrain@gmail.com>
2024-05-17 18:30:07 +0900

    first commit

--- overtime_v1.1/emp_overtime_input.py
+++ overtime_v1.1/emp_overtime_input.py
@@ -21,29 +21,29 @@ def resource_path(relative_path):
 
 #UI파일 연결
 # main_window= uic.loadUiType(resource_path("/Users/black/projects/make_erp/main_window.ui"))[0] # Mac 사용시 ui 주소
-emp_main_window= uic.loadUiType(resource_path("C:\\myproject\\python project\\overtime\\overtime_v1.1\\ui\\emp_search.ui"))[0] # Window 사용시 ui 주소
+emp_overtime_input_window= uic.loadUiType(resource_path("C:\\myproject\\python project\\overtime\\overtime_v1.1\\ui\\emp_overtime_input.ui"))[0] # Window 사용시 ui 주소
 dept_window = uic.loadUiType(resource_path("C:\\myproject\\python project\\overtime\\overtime_v1.1\\ui\\dept_window.ui"))[0]
 emp_window = uic.loadUiType(resource_path("C:\\myproject\\python project\\overtime\\overtime_v1.1\\ui\\emp_window.ui"))[0]
 
 # dial_window= uic.loadUiType(resource_path("C:\\myproject\\python project\\overtime\\popup_dept_info.ui"))[0] # Window 사용시 ui 주소
 
 #화면을 띄우는데 사용되는 Class 선언
-class EmpMainWindow(QWidget, emp_main_window) :
+class MainWindow(QWidget, emp_overtime_input_window) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("사원별 잔업시간 조회")
+        self.setWindowTitle("사원 잔업시간 입력")
         self.slots()
         # self.date_edit.setDate(QDate.currentDate())
         # self.date = self.date_edit.date().toString("yyyyMMdd")
 
     def slots(self):
-        self.btn_search.clicked.connect(self.make_data)
+        # self.btn_search.clicked.connect(self.make_data)
         self.btn_close.clicked.connect(self.window_close)
         self.btn_select_dept.clicked.connect(self.popup_dept_info)
         self.btn_select_emp.clicked.connect(self.popup_emp_info)
-        self.btn_clear.clicked.connect(self.clear)
-        self.btn_download.clicked.connect(self.make_file)
+        self.btn_input.clicked.connect(self.input_data)
+        # self.btn_clear.clicked.connect(self.clear)
 
     # def set_date(self):
     #     date = self.date_select.date()
@@ -98,32 +98,55 @@ class EmpMainWindow(QWidget, emp_main_window) :
         else:
             self.msg_box("입력오류", "입력값을 확인 하세요.")
 
-    def make_table(self, num, arr_1, title):   
-        self.tbl_info.setRowCount(0) # clear()는 행은 그대로 내용만 삭제, 행을 "0" 호출 한다.
+    def input_data(self):
 
-        col = len(title)
+        overtime_date = self.date.date()
+        overtime_date = overtime_date.toString("yyyy-MM-dd")
 
-        self.tbl_info.setRowCount(num)
-        self.tbl_info.setColumnCount(col)
+        dept_id = self.txt_dept_id.toPlainText()
+        dept_name = self.txt_dept_name.toPlainText()
+        emp_id = self.txt_emp_id.toPlainText()
+        emp_name = self.txt_emp_name.toPlainText()
+        overtime = self.txt_overtime.toPlainText()
+        from_time = self.txt_from_time.toPlainText()
+        to_time = self.txt_to_time.toPlainText()
+        detail = self.txt_detail.toPlainText()
+        note = self.txt_note.toPlainText()
+
+        list = [dept_id, dept_name, emp_id, emp_name, overtime_date, str(overtime), from_time, to_time, detail, note]
+        title = ["부서ID", "부서명", "사번", "사원명", "잔업일자", "잔업시간", "시작시간", "종료시간", "작업내용", "비고"]
+        
+        # 필수 입력값 
+        list_must = [dept_id, dept_name, emp_id, emp_name, overtime_date, str(overtime)]
+        title_must = ["부서ID", "부서명", "사번", "사원명", "잔업일자", "잔업시간"]
+
+        # 필수 입력값 중 하나라도 ""이면 처리 중단
+        for i, value in enumerate(list_must, start=1):
+            if value == "":
+                self.msg_box("입력오류", f"{title_must[i-1]} 값이 누락 됐습니다.")
+                return
+
+        row_count = self.tbl_info.rowCount()
+        col_count = len(title)
+        
+        self.tbl_info.insertRow(row_count)
+        self.tbl_info.setColumnCount(col_count)
         self.tbl_info.setHorizontalHeaderLabels(title)
 
-        for i in range(num):
-            for j in range(col): # 아니면 10개
-                self.tbl_info.setItem(i, j, QTableWidgetItem(str(arr_1[i][j])))
-                self.tbl_info.item(i, j).setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)     
+        for i in range(col_count):
+            self.tbl_info.setItem(row_count, i, QTableWidgetItem(str(list[i])))
+            self.tbl_info.item(row_count, i).setTextAlignment(Qt.AlignCenter | Qt.AlignVCenter)
 
-        # 컨텐츠의 길이에 맞추어 컬럼의 길이를 자동으로 조절
-        ################################################################
-        table = self.tbl_info
-        header = table.horizontalHeader()
+            self.txt_dept_id.setText("")
+            self.txt_dept_name.setText("")
+            self.txt_emp_id.setText("")
+            self.txt_emp_name.setText("")
+            self.txt_overtime.setText("")
+            self.txt_from_time.setText("")
+            self.txt_to_time.setText("")
+            self.txt_detail.setText("")
+            self.txt_note.setText("")
 
-        for i in range(col):
-            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
-        ################################################################
-
-        # 테이블의 길이에 맞추어 컬럼 길이를 균등하게 확장
-        self.tbl_info.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-    
     # 테이블 선택범위 삭제
     def delete_rows(self):
         indexes = []
@@ -190,77 +213,6 @@ class EmpMainWindow(QWidget, emp_main_window) :
     def get_dept_id(self):
         return self.dept_id
         
-     # 테이블에 남겨진 정보를 엑셀로 변환
-    def make_file(self):
-        rows = self.tbl_info.rowCount()
-        cols = self.tbl_info.columnCount()
-
-        list_2 = [] # 최종적으로 사용할 리스트는 for문 밖에 선언
-        for i in range(rows):
-            list_1 = [] # 2번째 for문 안쪽에서 사용할 리스트 선언
-            for j in range(cols): 
-                data = self.tbl_info.item(i,j)
-                list_1.append(data.text())
-            list_2.append(list_1)
-        
-        num = len(list_2)
-        self.make_excel(list_2, num)
-
-    # 엑셀 파일을 만들고 넘겨진 배열 정보를 이용하여 sheet에 정보를 기입/저장 함.
-    def make_excel(self, arr, num):
-        wb = openpyxl.Workbook()
-        wb.create_sheet(index=0, title='잔업정보')
-
-        sheet = wb.active
-        list_line = ["부서명", "사원명", "날짜", "잔업시간"]
-        sheet.append(list_line)
-
-        for i in range(num):
-            for j in range(len(list_line)):
-                sheet.cell(row=i+2, column=j+1, value=arr[i][j])
-
-        ## 각 칼럼에 대해서 모든 셀값의 문자열 개수에서 1.1만큼 곱한 것들 중 최대값을 계산한다.
-        for column_cells in sheet.columns:
-            # length = max(len(str(cell.value))*1.1 for cell in column_cells)
-            sheet.column_dimensions[column_cells[0].column_letter].width = 20
-            ## 셀 가운데 정렬
-            for cell in sheet[column_cells[0].column_letter]:
-                cell.alignment = Alignment(horizontal='center')
-        
-        fname = self.file_save()
-
-        try:
-            if fname:
-                self.save_excel(wb, fname)
-        except Exception as e:
-            self.msg_box("Error", str(e))
-
-
-    # 파일 저장 대화상자
-    def file_save(self):
-        now = datetime.now()
-        arg_1 = now.strftime('%Y-%m-%d %H-%M-%S')
-        adress = "./excel/download_" + arg_1 + ".xlsx"
-
-        dialog = QFileDialog(self)
-        qurl  = dialog.getSaveFileName(parent=self, caption='Save file', directory=adress)
-        
-        url = qurl[0]
-        try:
-            return url
-        except Exception as e:
-            QMessageBox.about(self, 'Warning', e)
-
-
-    def save_excel(self, workbook, file_name):
-        workbook.save(file_name)
-
-        self.close()
-
-    # # def dept_name(self, arg_1):  
-    # #     self.txt_dept_id.setText("arg_1.text()")
-    # #     print(arg_1)
-
     def msg_box(self, arg_1, arg_2):
         msg = QMessageBox()
         msg.setWindowTitle(arg_1)               # 제목설정
@@ -403,6 +355,6 @@ class EmpWindow(QDialog, emp_window):
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
-    myWindow = EmpMainWindow()
+    myWindow = MainWindow()
     myWindow.show()
     app.exec_()
