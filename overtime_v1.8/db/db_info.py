f582be859931c153e224482e41f64f871496bed2
myholyrain <myholyrain@gmail.com>
2025-01-02 08:55:45 +0900

    first commit

---
+++ overtime_v1.8/db/db_info.py
@@ -0,0 +1,23 @@
+class Connect:
+
+    # # test server
+    # host = "125.143.154.68"
+    # port = 3307
+    # database = "webserver"
+    # username = "scott"
+    # password = "!Jhlee0809"        
+
+    # doochpump 화성공장 hr server 
+    host = "106.248.19.234"
+    port = 3306
+    database = "webserver"
+    username = "scott"
+    password = "!Jhlee0809" 
+           
+        
+    # # wms-server
+    # host = "192.168.1.197"
+    # port = 3306
+    # database = "webserver"
+    # username = "scott"
+    # password = "!Jhlee0809"    
